e82e56368c64ade10d822ac75081ff8b26e12d76
Ashia Lewis <60453253+AeRabelais@users.noreply.github.com>
2021-10-21 09:58:57 -0500

    Create extract_images.py
    Create python file used to extract the tb and select healthy patient images

---
+++ scripts/data_extraction/extract_images.py
@@ -0,0 +1,17 @@
+import os
+import csv
+
+padchest_zips = ' ' #path where the padchest zip folders are held
+
+for file_name in os.listdir(padchest_zips):
+  if(file_name.endswith('.zip'):
+     zip_file = padchest_zips + '/' + filename
+     with ZipFile(zip_file, 'r') as zipObject:
+         listOfFileNames = zipObject.namelist()
+         for img in listOfFileNames:
+            if img in imgids
+     
+     
+     #update the remains
+     
+     
